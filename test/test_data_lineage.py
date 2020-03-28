e8b8f448df20f2cb49740c8ffa1d061189d150dc
Rajat Venkatesh <vrajat@dblint.io>
2020-03-28 14:40:50 +0530

    feature: Parse and visit a list of queries
    Add helper functions that accept an array of queries, parse them and use
    visitors to detect DML queries.

--- test/test_data_lineage.py
+++ test/test_data_lineage.py
@@ -1,11 +1,70 @@
-from data_lineage.data_lineage import fib
+from data_lineage.catalog.query import Query
+from data_lineage.data_lineage import parse, dml_queries
+
+queries = [
+    """
+    INSERT INTO page_lookup_nonredirect 
+            SELECT  page.page_id as redircet_id, page.page_title as redirect_title, page.page_title true_title, 
+                    page.page_id, page.page_latest 
+            FROM page LEFT OUTER JOIN redirect ON page.page_id = redirect.rd_from
+            WHERE redirect.rd_from IS NULL
+    """,
+    """
+    insert into page_lookup_redirect 
+            select original_page.page_id redirect_id, original_page.page_title redirect_title, 
+                    final_page.page_title as true_title, final_page.page_id, final_page.page_latest 
+            from page final_page join redirect on (redirect.page_title = final_page.page_title) 
+                join page original_page on (redirect.rd_from = original_page.page_id)
+    """,
+    """
+    INSERT INTO page_lookup
+            SELECT redirect_id, redirect_title, true_title, page_id, page_version
+            FROM (
+                SELECT redirect_id, redirect_title, true_title, page_id, page_version
+                FROM page_lookup_nonredirect
+                UNION ALL
+                SELECT redirect_id, redirect_title, true_title, page_id, page_version
+                FROM page_lookup_redirect) u
+    """,
+    """
+           INSERT INTO filtered_pagecounts 
+           SELECT regexp_replace (reflect ('java.net.URLDecoder','decode', reflect ('java.net.URLDecoder','decode',pvs.page_title)),'^\s*([a-zA-Z0-9]+).*','$1') page_title 
+                ,SUM (pvs.views) AS total_views, SUM (pvs.bytes_sent) AS total_bytes_sent
+            FROM pagecounts as pvs 
+           WHERE not pvs.page_title LIKE '(MEDIA|SPECIAL||Talk|User|User_talk|Project|Project_talk|File|File_talk|MediaWiki|MediaWiki_talk|Template|Template_talk|Help|Help_talk|Category|Category_talk|Portal|Wikipedia|Wikipedia_talk|upload|Special)\:(.*)' and
+                pvs.page_title LIKE '^([A-Z])(.*)' and
+                not pvs.page_title LIKE '(.*).(jpg|gif|png|JPG|GIF|PNG|txt|ico)$' and
+                pvs.page_title <> '404_error/' and 
+                pvs.page_title <> 'Main_Page' and 
+                pvs.page_title <> 'Hypertext_Transfer_Protocol' and 
+                pvs.page_title <> 'Favicon.ico' and 
+                pvs.page_title <> 'Search' and 
+                pvs.dt = '2020-01-01'
+          GROUP BY 
+                regexp_replace (reflect ('java.net.URLDecoder','decode', reflect ('java.net.URLDecoder','decode',pvs.page_title)),'^\s*([a-zA-Z0-9]+).*','$1')
+    """,
+    """
+    INSERT INTO normalized_pagecounts
+           SELECT pl.page_id page_id, REGEXP_REPLACE(pl.true_title, '_', ' ') page_title, pl.true_title page_url, views, bytes_sent
+           FROM page_lookup pl JOIN filtered_pagecounts fp 
+           ON fp.page_title = pl.redirect_title where fp.dt='2020-01-01'
+    """
+]
 
 
-def test_fib() -> None:
-    assert fib(0) == 0
-    assert fib(1) == 1
-    assert fib(2) == 1
-    assert fib(3) == 2
-    assert fib(4) == 3
-    assert fib(5) == 5
-    assert fib(10) == 55
+def test_parser():
+    query_objs = [Query(q) for q in queries]
+
+    parsed = parse(query_objs)
+    assert len(parsed) == len(queries)
+
+
+def test_visitor():
+    query_objs = [Query(q) for q in queries]
+    parsed = parse(query_objs)
+
+    dml = dml_queries(parsed)
+    assert len(dml) == len(queries)
+
+    for d in dml:
+        assert len(d.sources) > 0 and d.target is not None
